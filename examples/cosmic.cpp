ddde048c33ccc73b4e97200140a2e29808217a2e
Adam McLaughlin <adam27x@gmail.com>
2020-11-13 18:34:13 -0500

    Add advanced validation example

--- examples/cosmic.cpp
+++ examples/cosmic.cpp
@@ -52,14 +52,105 @@ int main(int, char *argv[])
 	std::cout << "Button text: " << button_text << "\n";*/
 
 	//Binding to events
-	Tk::label(".l") -Tk::text("Starting...");
+	/*Tk::label(".l") -Tk::text("Starting...");
 	Tk::grid(Tk::configure, ".l");
 	Tk::bind(".l", "<Enter>", [] () { ".l" << Tk::configure() -Tk::text("Moved mouse inside"); });
 	Tk::bind(".l", "<Leave>", [] () { ".l" << Tk::configure() -Tk::text("Moved mouse outside"); }); 
 	Tk::bind(".l", "<ButtonPress-1>", [] () { ".l" << Tk::configure() -Tk::text("Clicked left mouse button"); }); 
 	Tk::bind(".l", "<3>", [] () { ".l" << Tk::configure() -Tk::text("Clicked right mouse button"); }); 
 	Tk::bind(".l", "<Double-1>", [] () { ".l" << Tk::configure() -Tk::text("Double clicked left mouse button"); });
-	Tk::bind(".l", "<B3-Motion>", [] (int x, int y) { std::stringstream msg; msg << "Right button drag to " << x << " " << y; ".l" << Tk::configure() -Tk::text(msg.str()); }, Tk::event_x, Tk::event_y);
+	Tk::bind(".l", "<B3-Motion>", [] (int x, int y) { std::stringstream msg; msg << "Right button drag to " << x << " " << y; ".l" << Tk::configure() -Tk::text(msg.str()); }, Tk::event_x, Tk::event_y);*/
+
+	//Validation of entries
+	/*std::string zip;
+	Tk::entry(".e") -Tk::textvariable(zip) -Tk::validate(Tk::key) -Tk::validatecommand([] (const std::string &n) { return std::all_of(n.begin(),n.end(),::isdigit) && n.size() <= 5; }, Tk::valid_P);
+	Tk::grid(Tk::configure, ".e") -Tk::column(0) -Tk::row(0) -Tk::sticky("we");*/
+
+	//Advanced validation example
+	/*Tk::frame(".f");
+	Tk::grid(Tk::configure, ".f") -Tk::column(0) -Tk::row(0);
+	Tk::label(".f.l1") -Tk::text("Name:");
+	Tk::grid(Tk::configure, ".f.l1") -Tk::column(0) -Tk::row(0) -Tk::padx(5) -Tk::pady(5);
+	Tk::entry(".f.e1");
+	Tk::grid(Tk::configure, ".f.e1") -Tk::column(1) -Tk::row(0) -Tk::padx(5) -Tk::pady(5);
+	Tk::label(".f.l") -Tk::text("Zip:");
+	Tk::grid(Tk::configure, ".f.l") -Tk::column(0) -Tk::row(1) -Tk::padx(5) -Tk::pady(5);
+
+	std::string zip;
+	std::string errmsg;
+	auto callback = [&errmsg] (const std::string &newval, const std::string &op)
+	{
+		errmsg = std::string("");
+		bool valid = false;
+		std::string first_zip;
+		if(newval.size() >= 5)
+		{
+			first_zip = std::string(newval.begin(),newval.begin()+5);
+		}
+		else
+		{
+			valid = false;
+		}
+		if(newval.size() == 5 && std::all_of(first_zip.begin(),first_zip.end(),::isdigit))
+		{
+			valid = true;
+		}
+		std::string second_zip;
+		if(newval.size() == 10)
+		{
+			second_zip = std::string(newval.begin()+6,newval.end());
+			if(std::all_of(first_zip.begin(),first_zip.end(),::isdigit) && std::all_of(second_zip.begin(),second_zip.end(),::isdigit) && newval[5] == '-')
+			{
+				valid = true;
+			}
+		}
+
+		if(valid)
+		{
+			".f.btn" << Tk::configure() -Tk::state(Tk::normal);
+		}
+		else
+		{
+			".f.btn" << Tk::configure() -Tk::state(Tk::disabled);
+		}
+
+		if(op.compare("key") == 0)
+		{
+			//Check if valid so far
+			bool ok_so_far = false;
+			if(newval.size() <= 10)
+			{
+				if(std::all_of(newval.begin(),newval.end(),[] (char c) { if((c >= '0' && c <= '9') || c == '-') return true; return false; }))
+				{
+					ok_so_far = true;
+				}
+			}
+
+			if(!ok_so_far)
+			{
+				errmsg = "Zip should be ##### or #####-####";
+			}
+			return ok_so_far;
+		}
+		else if(op.compare("focusout") == 0)
+		{
+			//Send error if not valid
+			if(!valid)
+			{
+				errmsg = "Zip should be ##### or #####-####";
+			}
+		}
+
+		return valid;
+	};
+
+	Tk::entry(".f.e") -Tk::textvariable(zip) -Tk::validate(Tk::all) -Tk::validatecommand(callback, Tk::valid_P, Tk::valid_V);
+	Tk::grid(Tk::configure,".f.e") -Tk::column(1) -Tk::row(1) -Tk::padx(5) -Tk::pady(5);
+	Tk::button(".f.btn") -Tk::text("Process");
+	Tk::grid(Tk::configure, ".f.btn") -Tk::column(2) -Tk::row(1) -Tk::padx(5) -Tk::pady(5);
+	".f.btn" << Tk::configure() -Tk::state(Tk::disabled);
+	Tk::label(".f.msg") -Tk::font("SmallCaptionFont") -Tk::foreground("red") -Tk::textvariable(errmsg);
+	Tk::grid(Tk::configure, ".f.msg") -Tk::column(1) -Tk::row(2) -Tk::padx(5) -Tk::pady(5) -Tk::sticky("w");*/
 
      	Tk::runEventLoop();
 }
