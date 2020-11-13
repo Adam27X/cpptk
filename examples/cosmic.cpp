88026568aaa63399ca85e919ba7fd9ff0983a18c
Adam McLaughlin <adam27x@gmail.com>
2020-11-12 19:36:36 -0500

    Some more tutorials

--- examples/cosmic.cpp
+++ examples/cosmic.cpp
@@ -10,7 +10,8 @@ int main(int, char *argv[])
 	/*Tk::button(".b") -text("Hello World");
 	Tk::grid(Tk::configure,".b");*/
 
-	Tk::wm(Tk::title, ".", "Feet to meters"); //Change the title of the main window
+	//Second tutorial
+	/*Tk::wm(Tk::title, ".", "Feet to meters"); //Change the title of the main window
 	Tk::frame(".c") -Tk::padx(3) -Tk::pady(12); //FIXME: How to support padding("3 3 12 12")?
 	Tk::grid(Tk::configure, ".c") -Tk::column(3) -Tk::row(2) -Tk::sticky("nsew");
 	Tk::grid(Tk::columnconfigure, ".", 0) -Tk::weight(1);
@@ -36,7 +37,29 @@ int main(int, char *argv[])
 
 	Tk::grid(Tk::configure, Tk::winfo(Tk::children, ".c")) - Tk::padx(5) -Tk::pady(5);
 	Tk::focus(".c.feet");
-	Tk::bind(".", "<Return>", calculate);
+	Tk::bind(".", "<Return>", calculate);*/
+
+	//Configuration options
+	/*std::function<void()> button_pressed = [] {};
+	Tk::button(".b") -Tk::text("Hello") -Tk::command(button_pressed);
+
+	std::string button_text(".b" << Tk::cget(Tk::text));
+	std::string command(".b" << Tk::cget(Tk::command));
+	std::cout << "Button text: " << button_text << "\n";
+	std::cout << "Button cmd: " << command << "\n";
+	".b" << Tk::configure() -Tk::text("Goodbye");
+	button_text = std::string(".b" << Tk::cget(Tk::text));
+	std::cout << "Button text: " << button_text << "\n";*/
+
+	//Binding to events
+	Tk::label(".l") -Tk::text("Starting...");
+	Tk::grid(Tk::configure, ".l");
+	Tk::bind(".l", "<Enter>", [] () { ".l" << Tk::configure() -Tk::text("Moved mouse inside"); });
+	Tk::bind(".l", "<Leave>", [] () { ".l" << Tk::configure() -Tk::text("Moved mouse outside"); }); 
+	Tk::bind(".l", "<ButtonPress-1>", [] () { ".l" << Tk::configure() -Tk::text("Clicked left mouse button"); }); 
+	Tk::bind(".l", "<3>", [] () { ".l" << Tk::configure() -Tk::text("Clicked right mouse button"); }); 
+	Tk::bind(".l", "<Double-1>", [] () { ".l" << Tk::configure() -Tk::text("Double clicked left mouse button"); });
+	Tk::bind(".l", "<B3-Motion>", [] (int x, int y) { std::stringstream msg; msg << "Right button drag to " << x << " " << y; ".l" << Tk::configure() -Tk::text(msg.str()); }, Tk::event_x, Tk::event_y);
 
      	Tk::runEventLoop();
 }
